bcec92ffe385b02ca77a7e858237b16e0081d3f5
Koen Rutten <Ruttenk@gmail.com>
2015-12-08 10:17:35 +0100

    Start of project

---
+++ src/ibm/com/nl/HelloWorld.java
@@ -0,0 +1,13 @@
+package ibm.com.nl;
+
+public class HelloWorld {
+private String message;
+
+public String getMessage() {
+	return message;
+}
+
+public void setMessage(String message) {
+	this.message = message;
+}
+}
---
+++ src/ibm/com/nl/SpringMain.java
@@ -0,0 +1,15 @@
+package ibm.com.nl;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringMain {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext factory = new ClassPathXmlApplicationContext("Beans.xml");
+		HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+		System.out.println(obj.getMessage());
+
+	}
+
+}
